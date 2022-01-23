a0ce69e10f489c5ef487ec43281e2943d19d1325
Максим Водянов <max.post@bk.ru>
2022-01-24 02:27:41 +0300

    change folder camelcase

---
+++ src/pages/Chat/index.ts
@@ -0,0 +1,36 @@
+import template from './Chat.pug';
+import { VALIDATION_RULES } from '../../utils/validation';
+import Block from '../../utils/Block';
+import { validateFormControls } from '../../utils/events';
+import FormControl from '../../components/FormControl';
+import Button from '../../components/Button';
+import ChatListItem from '../../components/ChatListItem';
+
+export default class Chat extends Block {
+  constructor() {
+    super({
+      formControlMessage: new FormControl({
+        name: 'message',
+        placeholder: 'Сообщение...',
+        className: 'chat-content-control__input',
+        rule: VALIDATION_RULES.MESSAGE,
+      }),
+      button: new Button({
+        label: 'Отправить',
+        type: 'submit',
+        className: 'chat-content-control__button',
+        events: { click: (event: Event) => validateFormControls.call(this, event, this.children) },
+      }),
+      chatListItem: new ChatListItem({
+        author: 'Андрей',
+        content: 'Pug is sucks',
+        timestamp: '10:45',
+        count: 3,
+      }),
+    });
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
