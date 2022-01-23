12361fd1695847da7512064973c32feb02d41f0c
Максим Водянов <max.post@bk.ru>
2022-01-23 23:10:44 +0300

    added hashrouter

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,35 @@ import { Block } from './types/types';
 
 const app = document.querySelector('#app')!;
 
-function render(Page: Block) {
-  app.appendChild(Page.render());
-}
-
 const ROUTES = {
-  HOMEPAGE: '/',
-  LOGIN: '/login',
-  REGISTER: '/register',
-  CHAT: '/chat',
-  PROFILE: '/profile',
+  HOMEPAGE: '',
+  LOGIN: '#login',
+  REGISTER: '#register',
+  CHAT: '#chat',
+  PROFILE: '#profile',
   ERROR: {
-    404: '/error/404',
-    500: '/error/500',
+    404: '#error/404',
+    500: '#error/500',
   },
 };
 
-switch (window.location.pathname) {
-  case ROUTES.HOMEPAGE:
-  case ROUTES.LOGIN: render(new Login()); break;
-  case ROUTES.REGISTER: render(new Register()); break;
-  case ROUTES.CHAT: render(new Chat()); break;
-  case ROUTES.PROFILE: render(new Profile()); break;
-  case ROUTES.ERROR[500]: render(new Error(500)); break;
-  default: render(new Error(404));
-}
+const render = (Page: Block) => {
+  app.innerHTML = '';
+  app.appendChild(Page.render());
+};
+
+const hashHandler = () => {
+  const { hash } = window.location;
+  switch (hash) {
+    case ROUTES.HOMEPAGE:
+    case ROUTES.LOGIN: render(new Login()); break;
+    case ROUTES.REGISTER: render(new Register()); break;
+    case ROUTES.CHAT: render(new Chat()); break;
+    case ROUTES.PROFILE: render(new Profile()); break;
+    case ROUTES.ERROR[500]: render(new Error(500)); break;
+    default: render(new Error(404));
+  }
+};
+
+hashHandler();
+window.addEventListener('hashchange', hashHandler);
