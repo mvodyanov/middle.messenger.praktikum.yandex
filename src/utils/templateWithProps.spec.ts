12c9a7ea41ae8862515d999a9b4886aa6fed90d0
Максим Водянов <max.post@bk.ru>
2022-02-27 01:06:33 +0300

    add Block tests

--- src/utils/templateWithProps.spec.ts
+++ src/utils/templateWithProps.spec.ts
@@ -1,7 +1,8 @@
+import * as pug from 'pug';
 import { assert } from 'chai';
 import templateWithProps from './templateWithProps';
 
-const template = 'button(type="{{type}}" class="{{className}}").button {{label}}';
+const template = pug.render('button(type="{{type}}" class="{{className}}").button {{label}}');
 
 describe('templateWithProps', () => {
   it('Pug-шаблон рендерится с пропсами', () => {
@@ -9,7 +10,7 @@ describe('templateWithProps', () => {
       templateWithProps(template, {
         type: 'button', className: 'simple', label: 'кнопка',
       }),
-      'button(type="button" class="simple").button кнопка',
+      '<button class="simple button" type="button">кнопка</button>',
     );
   });
 });
