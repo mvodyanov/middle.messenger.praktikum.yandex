3128baed85597d05e3787232ab8bae80e28c5e0e
Максим Водянов <max.post@bk.ru>
2022-06-19 13:02:06 +0300

    add https

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 app.use(express.static(`${__dirname}/dist/`));
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening on port ${PORT}!`);
-});
+const https = require('https');
+const selfSigned = require('openssl-self-signed-certificate');
 
+const options = {
+  key: selfSigned.key,
+  cert: selfSigned.cert,
+};
+
+const serv = https.createServer(options, app);
+serv.listen(PORT, '0.0.0.0', () => {
+  // eslint-disable-next-line no-console
+  console.warn(`Application is started on https://localhost:${PORT}/`);
+});
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(`${__dirname}/dist/index.html`));
 });
